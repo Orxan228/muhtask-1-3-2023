d22d4ec6061c9726d417bf0ed4fa1b65d901c3c3
Orxan228 <7ltdajp.code.edu.az>
2023-03-02 18:14:05 +0400

    solved task2.cs

---
+++ task2cs/ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2 reqem daxil edirik");
+            int num = Convert.ToInt32(Console.ReadLine();
+            int[] points = new int[num];
+
+            for (int i = 0; i < num; i++) {
+                Console.Write("Ededin quvvetini yaz");
+                string pointStr = Console.ReadLine();
+                int point = Convert.ToInt32(pointStr);
+                points[i] = point;
+
+
+
+
+            }
+
+        }
+    }
---
+++ task2cspart2/task2fin/task2fin/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace task2fin
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter lenght of an array: ");
+            int arrLenght = Convert.ToInt32(Console.ReadLine());
+            double[] arr = new double[arrLenght];
+            double value = 0;
+            int count = 0;
+            double[] primeArr = new double[0];
+            double[] compoundArr = new double[0];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine("Enter a value " + (i + 1) + " : ");
+                value = Convert.ToDouble(Console.ReadLine());
+                while (value == 0 || value < 0)
+                {
+                    Console.WriteLine("The min value you can enter is 1 and The value you entered must be more than 0");
+                    Console.WriteLine("Enter a value " + (i + 1) + " : ");
+                    value = Convert.ToDouble(Console.ReadLine());
+                }
+                if (value > 0)
+                {
+                    arr[i] = value;
+                }
+            }
+            foreach (double item in arr)
+            {
+                int i = 2;
+                count = 0;
+                while(i < item) 
+                {
+                    if (item % i == 0)
+                    count++;
+                    i++;
+                    
+                }
+                if (count>0)
+                {
+                    Array.Resize(ref compoundArr, compoundArr.Length+1);
+                    compoundArr[compoundArr.Length-1] = item;
+                    
+                }
+                else
+                {
+                    Array.Resize(ref primeArr, primeArr.Length+1);
+                    primeArr[primeArr.Length-1] = item;
+                }
+            }
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("Compound numbers:");
+            foreach (double item in compoundArr)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("-------------------------");
+            Console.WriteLine("Prime numbers");
+            foreach (double item in primeArr)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
